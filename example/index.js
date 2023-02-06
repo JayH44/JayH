feb10b12a6f7f8468f36e0eddd1a23a764c089b8
JayH <slamhjh@gmail.com>
2023-02-06 13:46:18 +0900

    네비_모달_애니메이션수정

--- example/index.js
+++ example/index.js
@@ -1,15 +1,34 @@
 const btnSide = document.querySelector('.btnSide'),
   navBar = document.querySelector('nav'),
-  gnbBar = document.querySelector('nav ul');
+  gnbBar = document.querySelector('nav ul'),
+  btnModal = document.querySelector('.btnBox button'),
+  modalBg = document.querySelector('.modalBg'),
+  btnOptnModal = document.querySelector('.btnOptnModal');
 
 btnSide.addEventListener('click', () => {
   navBar.classList.add('on');
-  btnSide.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
 });
 
 navBar.addEventListener('click', (e) => {
   if (e.target === e.currentTarget) {
     navBar.classList.remove('on');
-    btnSide.style.backgroundColor = 'white';
   }
+});
+
+btnModal.addEventListener('click', () => {
+  modalBg.classList.add('hide');
+  setTimeout(() => {
+    modalBg.style.display = 'none';
+  }, 300);
+});
+
+btnOptnModal.addEventListener('click', () => {
+  modalBg.classList.remove('hide');
+  setTimeout(() => {
+    modalBg.style.display = 'block';
+  }, 300);
+});
+
+window.addEventListener('resize', () => {
+  if (this.innerWidth > 800) navBar.classList.remove('on');
 });
