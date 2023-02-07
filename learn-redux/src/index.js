9b82942dc4b62ec7906eb0e3ff496a8bee18ddda
JayH <slamhjh@gmail.com>
2023-02-07 16:37:39 +0900

    리덕스_프리젠테이셔녈+컨테이너 컴포넌트

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -13,6 +13,6 @@ const store = configureStore({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />)
+    <App />
   </Provider>
 );
