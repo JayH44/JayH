663658093876e4bf3154733d101eabf035992ce2
JayH <slamhjh@gmail.com>
2023-02-10 15:46:44 +0900

    인스타그램클론_프로파일변경기능구현

--- neppstagram/src/component/page/Profile.jsx
+++ neppstagram/src/component/page/Profile.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
+import { fetchCurrentUser } from '../../redux/user';
+import Button from '../common/Button';
 import ProfileBox from '../profile/ProfileBox';
 
 function Profile() {
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    if (window.confirm('로그아웃 하시겠습니까?')) {
+      localStorage.removeItem('access-token');
+      dispatch(fetchCurrentUser());
+      window.location.reload();
+    }
+  };
+
   return (
     <Container>
       <ProfileBox />
+      <Button onClick={handleLogout}>Logout</Button>
     </Container>
   );
 }
@@ -13,8 +26,10 @@ function Profile() {
 const Container = styled.div`
   flex: 1;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 20px;
 `;
 
 export default Profile;
