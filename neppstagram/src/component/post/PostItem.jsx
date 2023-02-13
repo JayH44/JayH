de0f5e42c5f494104feebfc15e41a6fcb136a8e5
JayH <slamhjh@gmail.com>
2023-02-13 11:55:18 +0900

    인스타그램클론_다중사진 슬라이드 스크롤 구현

---
+++ neppstagram/src/component/post/PostItem.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled, { css } from 'styled-components';
+
+function PostItem({ post }) {
+  const [idx, setIdx] = useState(0);
+  const [mouseDownX, setMouseDownX] = useState(0);
+  const [isDrag, setIsDrag] = useState(false);
+  const imgLength = post.img_list.length;
+
+  const handleMouseDown = (e) => {
+    setMouseDownX(e.pageX);
+  };
+
+  const handleMouseUp = (e) => {
+    if (mouseDownX - e.pageX > 0) {
+      setIdx(idx + 1);
+      setIsDrag(true);
+    } else if (mouseDownX - e.pageX < 0) {
+      setIdx(idx - 1);
+      setIsDrag(true);
+    } else {
+      setIdx(idx);
+      setIsDrag(false);
+    }
+  };
+
+  const handleClick = (e) => {
+    if (isDrag) e.preventDefault();
+  };
+
+  return (
+    <ImgBox onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>
+      <ImageWrapper idx={idx} multi={imgLength > 1}>
+        {imgLength > 0 && (
+          <Link to={'' + post.id} onClick={handleClick}>
+            {post.img_list.map((img, idx) => (
+              <img key={idx} src={img.url} alt='' />
+            ))}
+          </Link>
+        )}
+      </ImageWrapper>
+      <BtnBox idx={idx}>
+        {post.img_list.map((_, idx) => (
+          <button key={idx} onClick={() => setIdx(idx)}></button>
+        ))}
+      </BtnBox>
+    </ImgBox>
+  );
+}
+
+const ImgBox = styled.li`
+  width: 250px;
+  height: 250px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  position: relative;
+  overflow: hidden;
+  border: 1px solid ${({ theme }) => theme.colors.bd_color};
+
+  a {
+    display: flex;
+    flex-direction: row;
+    -webkit-user-drag: none;
+  }
+
+  a img {
+    width: 100%;
+    -webkit-user-drag: none;
+  }
+`;
+
+const ImageWrapper = styled.div`
+  ${({ multi }) =>
+    multi &&
+    css`
+      transform: translateX(-${({ idx }) => idx * 100}%);
+      transition: transform 0.5s;
+    `}
+`;
+
+const BtnBox = styled.div`
+  position: absolute;
+  z-index: 10;
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 0;
+
+  button {
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    border: none;
+    background-color: rgba(0, 0, 0, 0.7);
+    cursor: pointer;
+  }
+`;
+
+export default PostItem;
--- neppstagram/src/component/post/PostList.jsx
+++ neppstagram/src/component/post/PostList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { getPosts } from '../../api/auth';
+import PostItem from './PostItem';
 
 function PostList() {
   const [posts, setPost] = useState([]);
@@ -13,13 +13,7 @@ function PostList() {
   return (
     <Container>
       {posts.map((post) => (
-        <ImgBox key={post.id}>
-          {post.img_list.length > 0 && (
-            <Link to={'' + post.id}>
-              <img src={post.img_list[0].url} alt='' />
-            </Link>
-          )}
-        </ImgBox>
+        <PostItem key={post.id} post={post} />
       ))}
     </Container>
   );
@@ -28,20 +22,6 @@ function PostList() {
 const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
-`;
-
-const ImgBox = styled.li`
-  width: 250px;
-  height: 250px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-  border: 1px solid ${({ theme }) => theme.colors.bd_color};
-
-  img {
-    width: 100%;
-  }
 `;
 
 export default PostList;
